e327ca8fb92299a8486243a62bd870dca18f1c5c
Sayan Das <sayandasraja56@gmail.com>
2025-03-30 19:08:19 +0530

    correction

--- backend/routes/device.route.js
+++ backend/routes/device.route.js
@@ -1,22 +1,16 @@
 import express, { Router } from 'express';
-import { changeDeviceStatus, getDeviceControl, getDevicesByRoom, registerDevice, renameDevice, updateDeviceControl, validateDevices } from '../controllers/device.controller.js';
+import { registerDevice, renameDevice, validateDevices } from '../controllers/device.controller.js';
 import { authMiddleware } from '../middlewares/auth.js';
 
 const router = Router();
 
 router.post('/register/:roomId',authMiddleware, registerDevice); 
 
-router.get('/get/:roomId', authMiddleware, getDevicesByRoom);
 
 router.patch('/rename/:roomId/:deviceId', authMiddleware, renameDevice);
 
-router.patch('/sttus/:roomId/:deviceId', authMiddleware, changeDeviceStatus);
-
 router.post('/validatedevice', validateDevices);
 
-//this 2 are for esp 32
-router.get('/control', getDeviceControl);
-router.post('/control', updateDeviceControl);
 
 
 export default router;
